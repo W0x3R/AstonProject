36e80f01e8fe2a157c95573da2477d2835a44776
Stanislau Vyrski <vyr4376@yandex.ru>
2025-08-06 15:07:05 +0300

    feat: styles for modals and headers have been added. the behavior of modal window controls has been changed

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,11 +1,14 @@
 import { createPortal } from "react-dom"
 import styles from "./Modal.module.css"
 
-export const Modal = ({ children, className, onClick }) => {
+export const Modal = ({ children, className, onClick, isModalOpen }) => {
 	const modalRoot = document.getElementById("modal-root")
 
 	return createPortal(
-		<div className={`${styles.overlay} ${className}`} onClick={onClick}>
+		<div
+			className={`${styles.overlay} ${styles[className]} ${isModalOpen ? styles.open : ""}`}
+			onClick={onClick}
+		>
 			<div className={styles.modal} onClick={(e) => e.stopPropagation()}>
 				<button onClick={onClick} className={styles.modal__close}>
 					✕
--- src/widgets/LayoutHeader/Header.tsx
+++ src/widgets/LayoutHeader/Header.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Button } from "../../shared/ui/Button/Button"
 import { Modal } from "../../shared/ui/Modal/Modal"
+import headerLogo from "../../shared/assets/images/logo.webp"
 import styles from "./Header.module.css"
+import { controlBodyScroll } from "../../shared/lib/controlBodyScroll"
 
 export const Header = () => {
 	const [isModalOpen, setIsModalOpen] = useState(false)
@@ -10,19 +12,44 @@ export const Header = () => {
 
 	const handleModalClose = () => setIsModalOpen(false)
 
+	useEffect(() => {
+		if (isModalOpen) {
+			controlBodyScroll("hidden")
+		} else {
+			controlBodyScroll("visible")
+		}
+	}, [isModalOpen])
+
 	return (
 		<header className={styles.header}>
 			<div className="container">
-				<Button onClick={handleModalOpen}>О проекте</Button>
-				{isModalOpen && (
-					<Modal className={"header__modal"} onClick={handleModalClose}>
+				<div className={styles.wrapper}>
+					<a href="/">
+						<img
+							className={styles.header__logo}
+							src={headerLogo}
+							alt="logo"
+							width={40}
+							height={40}
+						/>
+					</a>
+
+					<Button onClick={handleModalOpen} className="header__popup-btn">
+						<span>О проекте</span>
+					</Button>
+
+					<Modal
+						className={"header__modal"}
+						onClick={handleModalClose}
+						isModalOpen={isModalOpen}
+					>
 						<h2>О проекте</h2>
 						<p>
 							Приложение для просмотра постов и комментариев, основанное на
 							публичном API JSONPlaceholder.
 						</p>
 					</Modal>
-				)}
+				</div>
 			</div>
 		</header>
 	)
