2970d51c535daccca84e772a79ce909a5e4b4984
Stanislau Vyrski <vyr4376@yandex.ru>
2025-08-10 19:21:57 +0300

    feat: added functionality to change the color of the spinner when changing themes

--- src/shared/ui/LoadingSpinner/LoadingSpinner.tsx
+++ src/shared/ui/LoadingSpinner/LoadingSpinner.tsx
@@ -1,14 +1,13 @@
 import styles from "./LoadingSpinner.module.css"
-import loadingSpinner from "../../assets/images/spinner.svg"
+import Spinner from "../../assets/images/spinner.svg?react"
 
 export const LoadingSpinner = ({ size }) => {
 	return (
 		<div className={styles.spinner__wrapper}>
-			<img
+			<Spinner
 				className={`${styles.spinner} ${styles[size]}`}
 				width={60}
 				height={60}
-				src={loadingSpinner}
 				alt=""
 			/>
 		</div>
