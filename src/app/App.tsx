2c2d0301267338c9517329bbe6a36ebfa3bfb318
Stanislau Vyrski <vyr4376@yandex.ru>
2025-08-11 21:27:00 +0300

    chore: used global class for theme switching classes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,23 @@
+import { useEffect, useState } from "react"
 import { MainLayout } from "../shared/layouts/MainLayout"
 import { PostList } from "../widgets/PostList/PostList"
 import { Header } from "../widgets/LayoutHeader/Header"
 import { Footer } from "../widgets/LayoutFooter/Footer"
 import { ThemeContext } from "../shared/lib/ThemeProvider"
-import { useEffect, useState } from "react"
+import { GLOBAL_CLASSES } from "../shared/constants/globalClasses"
 
 export const App = () => {
-	const [theme, setTheme] = useState("light")
+	const [theme, setTheme] = useState(GLOBAL_CLASSES.light)
 
-	const toggleTheme = () => setTheme(theme === "light" ? "dark" : "light")
+	const toggleTheme = () =>
+		setTheme(
+			theme === GLOBAL_CLASSES.light ?
+				GLOBAL_CLASSES.dark
+			:	GLOBAL_CLASSES.light
+		)
 
 	useEffect(() => {
-		document.body.classList.remove("light", "dark")
+		document.body.classList.remove(GLOBAL_CLASSES.light, GLOBAL_CLASSES.dark)
 		document.body.classList.add(theme)
 	}, [theme])
 
