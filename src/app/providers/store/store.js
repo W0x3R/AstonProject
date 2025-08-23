54e6ef07258301b837d9e8dc329f25391d98fff9
Stanislau Vyrski <vyr4376@yandex.ru>
2025-08-23 19:31:32 +0300

    feat: created and connected usersapi and createentityadapter for user

--- src/app/providers/store/store.js
+++ src/app/providers/store/store.js
@@ -3,20 +3,25 @@ import { postsApi } from "../../../entities/post/api/postsApi"
 import { commentsApi } from "../../../entities/comments/api/commentsApi"
 import { albumsApi } from "../../../entities/albums/api/albumsApi"
 import { todosApi } from "../../../entities/todos/api/todosApi"
+import { usersApi } from "../../../entities/users/model/api/usersApi"
 import postReducer from "../../../entities/post/model/slice/postSlice"
+import userReducer from "../../../entities/users/model/slice/userSlice"
 
 export const store = configureStore({
 	reducer: {
 		posts: postReducer,
+		users: userReducer,
 		[postsApi.reducerPath]: postsApi.reducer,
 		[commentsApi.reducerPath]: commentsApi.reducer,
 		[albumsApi.reducerPath]: albumsApi.reducer,
 		[todosApi.reducerPath]: todosApi.reducer,
+		[usersApi.reducerPath]: usersApi.reducer,
 	},
 	middleware: (getDefaultMiddleware) =>
 		getDefaultMiddleware()
 			.concat(postsApi.middleware)
 			.concat(commentsApi.middleware)
 			.concat(albumsApi.middleware)
-			.concat(todosApi.middleware),
+			.concat(todosApi.middleware)
+			.concat(usersApi.middleware),
 })
--- src/pages/UsersPage/UsersPage.tsx
+++ src/pages/UsersPage/UsersPage.tsx
@@ -1,22 +1,19 @@
-import { useEffect, useState } from "react"
-import { usersData } from "../../entities/users/model/usersData"
+import { useSelector } from "react-redux"
+import { useGetUsersQuery } from "../../entities/users/model/api/usersApi"
 import { withLoading } from "../../shared/lib/hoc/withLoading"
 import { UserList as OriginalUserList } from "../../widgets/UserList/UserList"
+import { selectAllUsers } from "../../entities/users/model/slice/userSlice"
 
 const UserListsWithLoading = withLoading(OriginalUserList)
 
 export const UsersPage = () => {
-	const [data, setData] = useState([])
-	const [isLoading, setIsLoading] = useState(true)
-
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			setData(usersData)
-			setIsLoading(false)
-		}, 500)
-
-		return () => clearTimeout(timer)
-	}, [])
-
-	return <UserListsWithLoading usersData={data} isLoading={isLoading} />
+	const { isLoading, error } = useGetUsersQuery()
+	const usersFromSlice = useSelector(selectAllUsers)
+	return (
+		<UserListsWithLoading
+			usersData={usersFromSlice}
+			isLoading={isLoading}
+			error={error}
+		/>
+	)
 }
