64e14541e1ee62da6c240fd8fa3f82aa33314e61
Stanislau Vyrski <vyr4376@yandex.ru>
2025-08-28 18:54:16 +0300

    feat: added the ability to delete a post

--- src/entities/post/api/postsApi.tsx
+++ src/entities/post/api/postsApi.tsx
@@ -31,6 +31,16 @@ export const postsApi = createApi({
 					]
 				:	[{ type: "Posts", id: "LIST" }],
 		}),
+		deletePost: builder.mutation({
+			query: (postId) => ({
+				url: `posts/${postId}`,
+				method: "DELETE",
+			}),
+			invalidatesTags: (result, error, id) => [
+				{ type: "Posts", id },
+				{ type: "Posts", id: "LIST" },
+			],
+		}),
 	}),
 })
 
