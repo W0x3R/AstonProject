c05fa0821dd25498dc2bf854fc3d33f80e99ef64
Stanislau Vyrski <vyr4376@yandex.ru>
2025-08-29 19:40:59 +0300

    feat: typed mainlayout

--- src/shared/layouts/MainLayout.tsx
+++ src/shared/layouts/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router"
+import type { TMainLayoutProps } from "./types"
 
-export const MainLayout = ({ header, footer }) => {
+export const MainLayout = ({ header, footer }: TMainLayoutProps) => {
 	return (
 		<>
 			{header}
