7c803a4de1664e552dd6cf55835753bce3e27914
Stanislau Vyrski <vyr4376@yandex.ru>
2025-08-29 19:43:37 +0300

    feat: children are typified

--- src/shared/ui/Container/Container.tsx
+++ src/shared/ui/Container/Container.tsx
@@ -1,5 +1,6 @@
+import type { ReactNode } from "react"
 import styles from "./Container.module.css"
 
-export const Container = ({ children }) => {
+export const Container = ({ children }: { children: ReactNode }) => {
 	return <div className={styles.container}>{children}</div>
 }
--- src/shared/ui/DataNotFound/DataNotFound.tsx
+++ src/shared/ui/DataNotFound/DataNotFound.tsx
@@ -1,7 +1,8 @@
+import type { ReactNode } from "react"
 import styles from "./DataNotFound.module.css"
 import errorImage from "../../assets/images/error.png"
 
-export const DataNotFound = ({ children }) => {
+export const DataNotFound = ({ children }: { children: ReactNode }) => {
 	return (
 		<div className={styles.wrapper}>
 			<img src={errorImage} alt="error-image" width={300} height={286} />
