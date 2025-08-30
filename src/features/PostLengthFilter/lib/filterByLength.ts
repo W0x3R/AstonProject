fdfd1ebff76ff73b7f4a4c4968fa8e107fe10dc4
Stanislau Vyrski <vyr4376@yandex.ru>
2025-08-30 18:22:58 +0300

    fix: added conversion to number type

--- src/features/PostLengthFilter/lib/filterByLength.ts
+++ src/features/PostLengthFilter/lib/filterByLength.ts
@@ -1,4 +1,4 @@
 import type { TFilterByLength } from "./types"
 
 export const filterByLength: TFilterByLength = (postsData, minLength) =>
-	postsData.filter((post) => post?.title.length >= minLength)
+	postsData.filter((post) => post?.title.length >= Number(minLength))
