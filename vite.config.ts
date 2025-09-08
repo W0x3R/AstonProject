f43f43a35bd29b984a798fbeadcedc349e65c3ab
Stanislau Vyrski <vyr4376@yandex.ru>
2025-09-08 21:50:39 +0300

    feat: add base path to config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), svgr()],
+	base: "/React-Intensive-Project/",
 	server: {
 		port: 3000,
 		open: true,
